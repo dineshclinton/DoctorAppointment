6b7f08385c92c877720bbc97136ceff3d7b48d44
Nikhil Mutalik <nikhil@firefly.health>
2020-04-24 15:24:49 -0400

    Initial commit

---
+++ backend/availabilities/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Availability(models.Model):
+    date = models.DateField()
+    start_time = models.TimeField()
+
+    @property
+    def end_time(self):
+        return self.start_time.timedelta(minutes=30)
+
+    class Meta:
+        ordering = ["date", "start_time"]
