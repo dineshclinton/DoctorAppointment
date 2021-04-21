4c5e55b707f5b4e62f88578cc77efc514c9a7113
Shivananda Sahu <shivananda.sahu@firefly.health>
2021-04-22 03:57:38 +0530

    Adding adming serializer and urls for appointment

--- backend/clinicians/admin.py
+++ backend/clinicians/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Clinician
+from .models import Appointment, Clinician
 
 
 class ClinicianAdmin(admin.ModelAdmin):
     pass
 
 
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Clinician, ClinicianAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
--- backend/clinicians/views.py
+++ backend/clinicians/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from clinicians.models import Clinician
 from rest_framework import viewsets
-from clinicians.serializers import ClinicianSerializer
+
+from clinicians.models import Appointment, Clinician
+from clinicians.serializers import AppointmentSerializer, ClinicianSerializer
+
 
 class ClinicianViewSet(viewsets.ModelViewSet):
     """
@@ -9,3 +11,11 @@ class ClinicianViewSet(viewsets.ModelViewSet):
     """
     queryset = Clinician.objects.all()
     serializer_class = ClinicianSerializer
+
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows appointments to be viewed or edited.
+    """
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
