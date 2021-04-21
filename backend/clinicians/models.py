dadfa40f562b6e80af7a91beecab0a93711861a6
Shivananda Sahu <shivananda.sahu@firefly.health>
2021-04-22 03:57:00 +0530

    Adding appointment object

--- backend/clinicians/models.py
+++ backend/clinicians/models.py
@@ -1,10 +1,42 @@
-from django.db import models
-
 from availabilities.models import Availability
+from django.db import models
 
 
 class Clinician(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     national_provider_identifier = models.CharField(max_length=10, unique=True)
-    availabilities = models.ManyToManyField(Availability)
+    availabilities = models.ManyToManyField(
+        Availability,
+        through='Appointment',
+        through_fields=(
+            'clinician',
+            'availability'
+        ),
+    )
+
+
+class AppointmentStatus:
+    AVAILABLE = "available"
+    BOOKED = "booked"
+
+
+APPOINTMENT_STATUS_CHOICES = [
+    (AppointmentStatus.AVAILABLE, "Available"),
+    (AppointmentStatus.BOOKED, "Booked"),
+]
+
+
+class Appointment(models.Model):
+    availability = models.ForeignKey(
+        Availability,
+        on_delete=models.CASCADE,
+    )
+    clinician = models.ForeignKey(
+        Clinician,
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        choices=APPOINTMENT_STATUS_CHOICES,
+        max_length=10
+    ) 
