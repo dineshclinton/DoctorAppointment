82c53044702c713e8aafc64c7bccf4a9867403de
Shivananda Sahu <shivananda.sahu@firefly.health>
2021-04-24 14:29:37 +0530

    Updating urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('availabilities.urls')),
     path('', include('clinicians.urls')),
+    path('', include('appointments.urls')),
     path('admin/', admin.site.urls),
 ]
